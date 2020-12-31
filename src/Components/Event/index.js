2502f27d171ba91e2846baea2c60df570fd1a8a1
Daniel Barben <daniel.barben@tamedia.ch>
2020-12-31 18:23:20 +0100

    final version

--- src/Components/Event/index.js
+++ src/Components/Event/index.js
@@ -20,7 +20,7 @@ open = () => {
     return (
     <li>
       <div className = "date" onClick = {() => this.open()}>{this.props.date} {this.state.visibility ?  <FaCaretUp /> : <FaCaretDown />}<h3 className = "headline">{this.props.headline}</h3></div>
-      <p className = {this.state.visibility ? "text visible" : "text hidden"}>{this.props.image !== "" ? <img src={gallery[this.props.image]} alt={this.props.headline} className="image"/> : ""}{this.props.text}<br/><a href={this.props.link} target="_blank">{this.props.link !== "" ? "... zum Artikel" : ""}</a>
+      <p className = {this.state.visibility ? "text visible" : "text hidden"}>{this.props.image !== "" ? <img src={gallery[this.props.image]} alt={this.props.headline} className="image"/> : ""}{this.props.text}<br/><a href={this.props.link} target="_blank" rel="noopener noreferrer">{this.props.link !== "" ? "... zum Artikel" : ""}</a>
       </p>
     </li>
   )}
